6ba34ae9c6f8e2877356f16fab4eaa9b727e8cc6
Sylor <sylorweb@gmail.com>
2019-11-28 00:06:19 +0400

    Add more tests

--- src/__tests__/decoder.test.ts
+++ src/__tests__/decoder.test.ts
@@ -1,4 +1,10 @@
-import { decodeBoolean, decodeNumber, decodeString } from '../decoder';
+import { decodeBoolean, decodeNumber, decodeObject, decodeString } from '../decoder';
+
+interface IUser {
+  name: string;
+  age: number;
+  hasJob: boolean;
+}
 
 test('decode boolean is success', () => {
   return expect(decodeBoolean(true)).resolves.toEqual(true);
@@ -23,3 +29,35 @@ test('decode string is success', () => {
 test('decode string is failed', () => {
   return expect(decodeString({})).rejects.toEqual('{} should be string');
 });
+
+test('decode object is success', () => {
+    const userData = {
+        age: 30,
+        hasJob: true,
+        name: 'Joe'
+    }
+    
+    const result = decodeObject<IUser>(userData, (data) => ({
+        age: decodeNumber(data.age),
+        hasJob: decodeBoolean(data.hasJob),
+        name: decodeString(data.name)
+    }));
+
+    return expect(result).resolves.toEqual(userData);
+  });
+  
+  test('decode object is failed', () => {
+    const userData = {
+        age: 30,
+        hasJob: 2,
+        name: 'Joe'
+    }
+    
+    const result = decodeObject<IUser>(userData, (data) => ({
+        age: decodeNumber(data.age),
+        hasJob: decodeBoolean(data.hasJob),
+        name: decodeString(data.name)
+    }));
+
+    return expect(result).rejects.toEqual('{\"age\":30,\"hasJob\":2,\"name\":\"Joe\"} is not valid: 2 should be boolean');
+  });
